8b273629794f89a4b18073d78ca9ab9d9182ca71
Hamilton <hamiltongamafilho@hotmail.com>
2024-01-20 17:40:43 -0300

    Create main.js

---
+++ assets/JS/main.js
@@ -0,0 +1,81 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelectorAll(".enemy"),
+        timeleft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+        lives: document.querySelector("#lives"),
+    }, 
+    values: {
+        timerId: null,
+        velocity:1000,
+        hitPosition:0,
+        result:0,
+        currentTime: 60,
+        currentlife: 3,
+    },
+    actions:{
+        countDownTimerId: setInterval(countDown, 1000),
+        timerId: setInterval(randomSquare, 1000),
+    }
+};
+
+function gameOver(){
+    alert("Fim de jogo! Seu resultado foi: " + state.values.result);
+    location.reload();
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`../assets/sounds/${audioName}.wav`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeleft.textContent = state.values.currentTime;
+    if (state.values.currentTime <= 0){
+        playSound("gameOver");
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.TimerId);
+        setTimeout(gameOver, 500);
+    }
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square)=>{
+        square.classList.remove("enemy");
+    })
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square)=>{
+        square.addEventListener("mousedown", ()=>{
+            if(square.id === state.values.hitPosition){
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }else {
+                state.values.currentlife--
+                state.view.lives.textContent = "x"+state.values.currentlife;
+                playSound("hit");
+                if(state.values.currentlife == 0){
+                    playSound("gameOver");
+                    setTimeout(gameOver, 500);
+                }
+            }
+        })
+    })
+};
+
+function initialize(){
+    addListenerHitBox();
+};
+
+initialize();
+
